2bf3a54da040601bfaa5d01f939e52db140347f7
ratsimisampy <ratsimisampy@yahoo.fr>
2020-04-25 21:50:22 +0300

    2nd step

--- app.rb
+++ app.rb
@@ -4,13 +4,18 @@ require 'restclient'
 
 begin
 #Opening a page with Nokogiri and open-uri
-page = Nokogiri::HTML(RestClient.get("https://www.paris.fr/services"))   
-page.xpath('//div[@class="paris-theme-card"]//a').each do |node|
-    puts node['href']
+page = Nokogiri::HTML(RestClient.get("https://coinmarketcap.com/all/views/all/")) 
+#autres var
+i = 0  
+result = Hash.new
+page.xpath('(//td[@class="cmc-table__cell cmc-table__cell--sortable cmc-table__cell--left cmc-table__cell--sort-by__symbol"]/div | //td[@class="cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__price"]/a)').each do |node|
+  
+    puts node.text
 end
 
-rescue => each
+rescue => err
 
     puts "en erreur !"
+    puts err
 
 end
